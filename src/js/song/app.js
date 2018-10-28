16599fcbe49772bc7029f2ec39c651a61615008d
adashuai5 <ad873606339@gmail.com>
2018-10-28 23:46:45 +0800

    背景模糊效果自动化

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -9,7 +9,7 @@
                 song,
                 status
             } = data
-            // this.$el.css('background-image', `url(${song.cover})`)
+            this.$el.find('.background').css('background-image', `url(${song.cover})`)
             this.$el.find('img.cover').attr('src', song.cover)
             if (this.$el.find('audio').attr('src') !== song.url) {
                 let audio = this.$el.find('audio').attr('src', song.url).get(0)
